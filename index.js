18fadf7047b126ec25e6b8d078f801980bb5d9d4
Altenia Gaines <gaines.tina90@gmail.com>
2023-04-12 17:15:56 -0700

    use filter methods

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (array,string){
+    return array.filter((driver) =>
+        driver.toUpperCase()=== string.toUpperCase());
+};
+
+function fuzzyMatch (array, string){
+    return array.filter((driver) =>
+        driver.slice(0,string.length) === string);
+} 
+function matchName (obj, string) {
+    return obj.filter(driver => driver.name === string)
+}
+console.log(findMatching())
